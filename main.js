2e470008218f213b597671cb9d184d369dbe2560
Agustina <dis.agustinalopez@gmail.com>
2022-08-31 22:57:25 -0300

    Subida a github Desafio Complementario 1

---
+++ main.js
@@ -0,0 +1,7 @@
+let numeroIngresado= Number( prompt ('Ingresá un número para multiplicarlo') );
+
+ for (let i = 1; i <= 10; i++) {
+    let resultado = numeroIngresado * i;
+   alert(numeroIngresado+ ' x '+i+ ' = '+resultado);
+}
+
